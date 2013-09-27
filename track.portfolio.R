6bce805ab5da626e1fe527b80672d690300a7de8
Alexander Dovzhikov <alexander.dovzhikov@excelian.com>
2013-09-27 15:36:37 +1000

    Redesign poertfolio.data with required columns and tracking report with new market data

--- track.portfolio.R
+++ track.portfolio.R
@@ -11,8 +11,10 @@ index.portfolio <- read.csv(file="index.portfolio.csv")
 stopifnot(as.character(index.portfolio$sec) == names(current.data))
 
 # calculate shares
+start.date <- "2013-09-02"
 money.share <- round(1e8 * index.portfolio$weight, digits=2)
-shares <- floor(money.share / current.data["2013-09-02",])
+shares <- floor(money.share / current.data[start.date,])
+buyPrices <- as.vector(series(current.data[start.date,]))
 
 # calculate portfolio prices
 price <- timeSeries(current.data %*% t(shares), charvec=rownames(current.data))
@@ -20,9 +22,11 @@ growth.value <- price - price[1]
 growth.rate <- log(price / price[1])
 
 # report
-portfolio.data <- cbind(Stock=as.character(index.portfolio$sec),
-                        Weight=index.portfolio$weight, 
-                        Shares=as.vector(t(shares)))
+portfolio.data <- cbind(Share=as.character(index.portfolio$sec),
+                        Price=buyPrices, 
+                        No.Shares=as.vector(t(shares)),
+                        Value=buyPrices * as.vector(t(shares)),
+                        Weight=index.portfolio$weight)
 tracking.report <- cbind(price=price, growth=growth.value, rate=growth.rate)
 
 write.csv(portfolio.data, file="portfolio.data.csv")
